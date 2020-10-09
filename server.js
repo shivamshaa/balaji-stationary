a2e38ee7a4f76f4a5e8444c68c8d977931247a11
Shivam Gupta <54493176+shivamshaa@users.noreply.github.com>
2020-10-10 00:25:12 +0530

    zzZ
    Zzz

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 let mailTransporter = nodemailer.createTransport({ 
-    host: 'smtp.gmail.com',
-    port: 465,
+    host: 'smtp.gmail.com', //smtp-mail.outlook.com
+    port: 465,              //587
     secure: true,
     logger: true,
     ignoreTLS: true, // add this  
     auth: { 
-        user: 'shaagupta04@gmail.com', 
-        pass: '@mitBT123'
-    } 
+        user: 'balajistationers26@gmail.com', 
+        pass: '26balaji4986'}
+    // } , tls: {
+    //     ciphers: 'SSLv3'
+    // }
 }); 
 
 
@@ -29,16 +31,16 @@ let mailTransporter = nodemailer.createTransport({
 app.post("/data",(req,res)=>{
    
     console.log(req.body.desig);
-    let txt = 'Dear' + req.body.fname + req.body.lname + 'your requested quotation for our product is attached in this mail' + 'your details as entered are' + 'phone: '+ req.body.phone
+    let txt = 'Dear' + req.body.fname +' '+ req.body.lname +  'your requested quotation for our product is attached in this mail' + 'your details as entered are' + 'phone: '+ req.body.phone
             + 'email: ' + req.body.email + 'company name: ' + req.body.cname + 'company address: '+ req.body.cloc + 'designation: ' + req.body.desig +
             'requested quote: link'
     let mailDetails = { 
         from: 'shaagupta04@gmail.com', 
         to: req.body.email, 
         subject: 'Test mail', 
-        text: 'Dear ' + req.body.fname + req.body.lname + ' your requested quotation for our product is attached in this mail ' + 'your details as entered are ' + 'phone: '+ req.body.phone
-        + ' email: ' + req.body.email + ' company name: ' + req.body.cname + ' company address: '+ req.body.cloc + ' designation: ' + req.body.desig +
-        ' requested quote: link'
+        text: 'Dear ' + req.body.fname +' '+ req.body.lname + ' your requested quotation for our products is attached in this mail, ' + 'your details as entered are ' + 'Phone No.: '+ req.body.phone
+        + ' Email Address: ' + req.body.email + ' Company Name: ' + req.body.cname + ' Company Location: '+ req.body.cloc + ' Designation: ' + req.body.desig +
+        ' Requested Quote: link'
     }; 
     mailTransporter.sendMail(mailDetails, function(err, data) { 
         if(err) { 
